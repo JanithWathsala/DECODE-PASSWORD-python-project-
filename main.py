fed3d8062a969ad0a1dc6248b297517cc9de08cb
Janith Wathsala <janijaniwathsala@gmail.com>
2024-05-25 18:42:27 +0530

    Write a code for decode continue to real password

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import tkinter as tk
 from tkinter import ttk
 import base64
 
+def add_task(event=None):
+    password = task_entry.get()
+    decoded_password = base64.b64decode(password).decode()
+    decode_num.config(text=f"Decoded code: {decoded_password}")
+    
 root=tk.Tk()
 root.geometry("400x400")
 root.title("DECODE APP")
@@ -15,14 +20,13 @@ frame.pack()
 task_entry = ttk.Entry(frame, font="arial 18", width=27)
 task_entry.pack()
 
-decode_button = ttk.Button(root, text="Encode")
+task_entry.bind("<Return>",add_task)
+
+decode_button = ttk.Button(root, text="Encode", command=add_task)
 decode_button.pack(pady=5)
 
 decode_num = ttk.Label(root, text="", font=("arial", 10))
 decode_num.pack()
-
-
-
 
 
 root.mainloop()
