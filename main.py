b9aa0177fe2117fa30c5fe25463ebf902b8a1972
Janith Wathsala <janijaniwathsala@gmail.com>
2024-05-25 18:55:17 +0530

    create a password copy button

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import tkinter as tk
 from tkinter import ttk
 import base64
+import pyperclip
 
 def add_task(event=None):
     password = task_entry.get()
     decoded_password = base64.b64decode(password).decode()
     decode_num.config(text=f"Decoded code: {decoded_password}")
     
+def copy_password():
+    password = task_entry.get()
+    decode_password = base64.b64decode(password).decode()
+    pyperclip.copy(decode_password)
+    
 root=tk.Tk()
-root.geometry("400x400")
+root.geometry("400x200")
 root.title("DECODE APP")
 
-ent_code = ttk.Label(root, text="Enter your code", font=("arial", 10, "bold"))
-ent_code.pack()
+de_code = ttk.Label(root, text="Enter your code", font=("arial", 10, "bold"))
+de_code.pack()
 
 frame = ttk.Frame(root, width=400, height=60)
 frame.pack()
@@ -22,11 +28,14 @@ task_entry.pack()
 
 task_entry.bind("<Return>",add_task)
 
-decode_button = ttk.Button(root, text="Encode", command=add_task)
+decode_button = ttk.Button(root, text="Decode", command=add_task)
 decode_button.pack(pady=5)
 
 decode_num = ttk.Label(root, text="", font=("arial", 10))
 decode_num.pack()
+
+copy_button = ttk.Button(root, text="Copy to password",command=copy_password)
+copy_button.pack(pady=5)
 
 
 root.mainloop()
